ad336a9f4de00dd8372a985f191cca5c8a118fbf
ainhoa32 <ainhoa32alarcon@gmail.com>
2025-05-21 09:13:26 +0200

    añadido token en todas las peticiones

--- src/servicios/ServicioCesta.js
+++ src/servicios/ServicioCesta.js
@@ -1,21 +1,35 @@
 import httpExterno from "./http-externo";
 
+const token = localStorage.getItem("token");
+
 class ServicioCesta {
+
   anadirProdCesta(producto) {
-    return httpExterno.post(`/cesta/agregar`, producto);
+    return httpExterno.post(`/cesta/agregar`, {
+      data: producto,
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
   }
 
   eliminarProdCesta(producto) {
     return httpExterno.delete(`/cesta/eliminar`, {
       data: producto,
       headers: {
+        "Authorization": `Bearer ${token}`,
         "Content-Type": "application/json",
       },
     });
   }
 
   getProdsCesta(user) {
-    return httpExterno.get(`/cesta/${user}`);
+    return httpExterno.get(`/cesta/${user}`, {
+      headers: {
+        "Authorization": `Bearer ${token}`,
+      },
+    });
   }
 }
 
