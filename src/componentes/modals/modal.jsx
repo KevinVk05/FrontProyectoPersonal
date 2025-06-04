085c01961b9e6dca5e4bb5ae710c5d070bfc1168
ainhoa32 <ainhoa32alarcon@gmail.com>
2025-06-04 11:55:22 +0200

    cambiada x modal

--- src/componentes/modals/modal.jsx
+++ src/componentes/modals/modal.jsx
@@ -7,8 +7,12 @@ const Modal = ({ isOpen, onClose, children }) => {
   return ReactDOM.createPortal(
     <div className="modal-overlay">
       <div className="modal-content m-4">
-        <button className="close-btn" onClick={onClose}>X</button>
-        {children}
+        <button className="btn btn-danger position-absolute top-0 end-0 m-2" onClick={onClose}><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="white" className="bi bi-x-lg" viewBox="0 0 16 16">
+  <path d="M2.146 2.146a.5.5 0 0 1 .708 0L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854a.5.5 0 0 1 0-.708z"/>
+</svg></button>
+        <div className='mt-3'>
+          {children}
+        </div>
       </div>
     </div>,
         document.body
